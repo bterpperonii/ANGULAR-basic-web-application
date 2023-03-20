396cdea67d6a098c79933e73a74e754ecc706b1c
maxime <maxime.curon@std.heh.be>
2023-03-20 21:54:00 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavComponent } from './view/nav/nav.component';
+import { StructuresComponent } from './view/structures/structures.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'nav', component: NavComponent },
+  { path: 'structures', component: StructuresComponent},
+  { path: '', redirectTo: 'nav', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './view/nav/nav.component';
+import { StructuresComponent } from './view/structures/structures.component';
+import { LayoutComponent } from './view/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    StructuresComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
